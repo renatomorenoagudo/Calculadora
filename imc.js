bf5158b52d6a4dcbe2d6b4ba16722357e981ece7
https://github.com/renatomorenoagudo <renatomorenoagudo2@gmail.com>
2025-01-04 22:47:58 -0300

    conditions if else

--- imc.js
+++ imc.js
@@ -2,14 +2,18 @@
 const calcular = document.getElementById ('calcular');
 
 function imc (){
-    const nome = document.getElementById ('nome');
-    const altura = document.getElementById ('altura');
-    const peso = document.getElementById ('peso');
+    const nome = document.getElementById ('nome').value;
+    const altura = document.getElementById ('altura').value;
+    const peso = document.getElementById ('peso').value;
     const resultado = document.getElementById ('resultado');
 
     //validação de campo preenchido:
-    if (nome.value !== ''){
-        alet ('nome vazio!')
+    if (nome.value !== '' && altura.value !=='' && peso !==''){
+        const valorImc =(peso/(altura * altura)).toFixed(2);
+        resultado.textContent = valorImc;
+
+    } else {
+        resultado.textContent = 'preencha todos campos!'
     }
 
 
